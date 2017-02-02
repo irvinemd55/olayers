46cf46def0b0b364c2173c7b178c5b2545900d52
jjron <jaren.escueta@gmail.com>
2017-02-01 23:38:14 -0800

    added more photo router tests

--- test/photo-router-test.js
+++ test/photo-router-test.js
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-// 'use strict';
-//
-// require('./mock-assets/mock-env.js');
-// const expect = require('chai').expect;
-// const superagent = require('superagent');
-// const Photo = require('../model/photo.js');
-// const userMock = require('./lib/user-mocks.js');
-// const profileMock = require('./lib/profile-mock.js');
-// const photoMock = require('./lib/photo-mock.js');
-// const serverControl = require('./lib/server-control.js');
-// const baseURL = `http://localhost:${process.env.PORT}`;
-// require('../server.js');
-//
-// describe('testing photo-router', function () {
-//   before(serverControl.startServer);
-//   after(serverControl.killServer);
-//   afterEach(done => {
-//     Photo.remove({})
-//     .then(() => done())
-//     .catch(done);
-//   });
-
-  // describe('testing POST /api/profile/:id/photo', function () {
-  //   before(userMock.bind(this));
-  //   before(profileMock.bind(this));
-  //   it.only('should respond with a photo', (done) => {
-  //     superagent.post(`${baseURL}/api/profile/${this.tempProfile._id.toString()}/photo`)
-  //     .send({
-  //       name: 'taco tuesday',
-  //       url: 'someurlstring.com',
-  //       dateTaken: new Date(),
-  //       userID: this.tempUser._id.toString(),
-  //     })
-  //     .set('Authorization',`Bearer ${this.tempToken}`)
-  //     .then(res => {
-  //       expect(res.status).to.equal(200);
-  //       expect(res.body.name).to.equal('taco tuesday');
-  //       expect(res.body.url).to.equal('someurlstring.com');
-  //
-  //       done();
-  //     })
-  //     .catch(done);
-  //   });
-  // });
-  //
-  // describe('testing POST /api/profile/:id/photo', function(){
-  //   beforeEach(userMock.bind(this));
-  //   beforeEach(profileMock.bind(this));
-  //   it.only('should return a photo model', (done) => {
-  //     superagent.post(`${baseURL}/api/profile/${this.tempProfile._id.toString()}/photo`)
-  //     .set('Authorization', `Bearer ${this.tempToken}`)
-  //     .field('profileID', this.tempProfile._id.toString())
-  //     .field('name', 'dog')
-  //     .attach('file', `${__dirname}/mock-assets/dog.jpg`)
-  //     .then(res => {
-  //       expect(res.status).to.equal(200);
-  //       expect(res.body.name).to.equal('dog');
-  //       expect(res.body.profileID).to.equal(this.tempProfile._id.toString());
-  //       expect(res.body.userID).to.equal(this.tempUser._id.toString());
-  //       expect(Boolean(res.body.url)).to.equal(true);
-  //       done();
-  //     })
-  //     .catch(done);
-  //   });
-  // });
-=======
 'use strict';
 
 require('./mock-assets/mock-env.js');
@@ -117,8 +50,62 @@ describe('testing photo-router', function () {
       })
       .catch(done);
     });
+
+    it('no attached file should respond with 400 no file', done => {
+      superagent.post(`${baseURL}/api/profiles/${this.tempProfile._id.toString()}/photos`)
+      .set('Authorization', `Bearer ${this.tempToken}`)
+      .field('profileID', this.tempProfile._id.toString())
+      .field('name', 'dog')
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(400);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('invalid token should respond with 401 unauthorized', done => {
+      superagent.post(`${baseURL}/api/profiles/${this.tempProfile._id.toString()}/photos`)
+      .set('Authorization', `Bearer badtoken`)
+      .field('profileID', this.tempProfile._id.toString())
+      .field('name', 'dog')
+      .attach('file', `${__dirname}/mock-assets/dog.jpg`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(401);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('bad endpoint should respond with 404 not found', done => {
+      superagent.post(`${baseURL}/api/profiles/${this.tempProfile._id.toString()}/oolala`)
+      .set('Authorization', `Bearer ${this.tempToken}`)
+      .field('profileID', this.tempProfile._id.toString())
+      .field('name', 'dog')
+      .attach('file', `${__dirname}/mock-assets/dog.jpg`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('invalid profileID should respond with 404 not found', done => {
+      superagent.post(`${baseURL}/api/profiles/42/photos`)
+      .set('Authorization', `Bearer ${this.tempToken}`)
+      .field('profileID', this.tempProfile._id.toString())
+      .field('name', 'dog')
+      .attach('file', `${__dirname}/mock-assets/dog.jpg`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
   });
->>>>>>> 52d75d6ff3bda14d7e00eae4a485f6caab3e478d
 
   describe('testing POST /api/posts/:id/photo', function(){
     beforeEach(userMock.bind(this));
@@ -140,6 +127,61 @@ describe('testing photo-router', function () {
       })
       .catch(done);
     });
+
+    it('no attached file should respond with 400 no file', done => {
+      superagent.post(`${baseURL}/api/posts/${this.tempPost._id.toString()}/photos`)
+      .set('Authorization', `Bearer ${this.tempToken}`)
+      .field('postID', this.tempPost._id.toString())
+      .field('name', 'dog')
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(400);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('invalid token should respond with 401 unauthorized', done => {
+      superagent.post(`${baseURL}/api/posts/${this.tempPost._id.toString()}/photos`)
+      .set('Authorization', `Bearer badtoken`)
+      .field('postID', this.tempPost._id.toString())
+      .field('name', 'dog')
+      .attach('file', `${__dirname}/mock-assets/dog.jpg`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(401);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('bad endpoint should respond with 404 not found', done => {
+      superagent.post(`${baseURL}/api/posts/${this.tempPost._id.toString()}/oolala`)
+      .set('Authorization', `Bearer ${this.tempToken}`)
+      .field('postID', this.tempPost._id.toString())
+      .field('name', 'dog')
+      .attach('file', `${__dirname}/mock-assets/dog.jpg`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('invalid postID should respond with 404 not found', done => {
+      superagent.post(`${baseURL}/api/posts/42/photos`)
+      .set('Authorization', `Bearer ${this.tempToken}`)
+      .field('postID', this.tempPost._id.toString())
+      .field('name', 'dog')
+      .attach('file', `${__dirname}/mock-assets/dog.jpg`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
   });
 
   describe('testing DELETE /api/photos/:id', function() {
@@ -149,26 +191,6 @@ describe('testing photo-router', function () {
     beforeEach(photoMock.bind(this));
 
     it('should delete a photo', (done) => {
-      console.log(`${baseURL}/api/photos/${this.tempPhoto._id.toString()}`);
-
-<<<<<<< HEAD
-  // describe('testing DELETE /api/photos', function(){
-  //   beforeEach(userMock.bind(this));
-  //   beforeEach(profileMock.bind(this));
-  //   beforeEach(photoMock.bind(this));
-  //
-  //   it('should delete a photo', (done) => {
-  //     superagent.delete(`${baseURL}/api/profile/${this.tempProfile._id.toString()}/photo`)
-  //     .set('Authorization', `Bearer ${this.tempToken}`)
-  //     .then( res => {
-  //       expect(res.status).to.equal(204);
-//   //       done();
-//   //     })
-//   //     .catch(done);
-//   //   });
-//   // });
-// });
-=======
       superagent.delete(`${baseURL}/api/photos/${this.tempPhoto._id.toString()}`)
       .set('Authorization', `Bearer ${this.tempToken}`)
       .then( res => {
@@ -177,6 +199,27 @@ describe('testing photo-router', function () {
       })
       .catch(done);
     });
+
+    it('invalid token provided should respond with 400', done => {
+      superagent.delete(`${baseURL}/api/photos/${this.tempPhoto._id.toString()}`)
+      .set('Authorization', `Bearer badtoken`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(401);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('invalid photoID should respond with 404', done => {
+      superagent.delete(`${baseURL}/api/photos/42`)
+      .set('Authorization', `Bearer ${this.tempToken}`)
+      .then(done)
+      .catch(err => {
+        expect(err.status).to.equal(404);
+        done();
+      })
+      .catch(done);
+    });
   });
 });
->>>>>>> 52d75d6ff3bda14d7e00eae4a485f6caab3e478d
